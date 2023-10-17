2249177bd33a23aba9580c11fed420f05155635e
Oussema Naffety <oussemanaffety@sfax.r-iset.tn>
2023-10-17 11:23:02 +0100

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-data-binding';
+  name = 'Mohamed';
+  etat_bouton = true;
+  etat_affichage = false;
+  compteur = 0; 
+
+  nameList = [
+    'Mohamed',
+    'Ali',
+    'Samia',
+    'Saleh',
+  ];
+  userList = [
+    { name: 'Mohamed', age: 10 },
+    { name: 'Ali', age: 20 },
+    { name: 'Samia', age: 12 },
+    { name: 'Saleh', age: 25 },
+  ];
+  afficher() {
+    this.etat_affichage = true;
+  }
+  toggleAffichage() {
+    this.etat_affichage = !this.etat_affichage;
+  }
+  increment() {
+    this.compteur += 1; 
+  }
+
+  decrement() {
+    this.compteur -= 1; 
+  }
 }
